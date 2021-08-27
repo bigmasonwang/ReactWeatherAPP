0ff11671fd1161c4d0ab4ac04511c16df03bb1a4
Meisen Wang <bigmasonwang@gmail.com>
2021-08-27 12:33:36 +0930

    🐞 fix(search bar): handle invalid search

--- src/components/WeatherScreen.js
+++ src/components/WeatherScreen.js
@@ -18,24 +18,32 @@ const WeatherScreen = () => {
           `http://api.openweathermap.org/geo/1.0/direct?q=${curCity}&appid=${apiKey}`
         )
         .then((res) => {
-          const lat = res.data[0].lat;
-          const lon = res.data[0].lon;
-          axios
-            .get(
-              `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=metric`
-            )
-            .then((res) => {
-              // console.log(res.data);
-              setCurWeather(res.data.current);
-              setDailyWeather(res.data.daily);
-            });
+          if (res.data[0]) {
+            const lat = res.data[0].lat;
+            const lon = res.data[0].lon;
+            axios
+              .get(
+                `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${apiKey}&units=metric`
+              )
+              .then((res) => {
+                // console.log(res.data);
+                setCurWeather(res.data.current);
+                setDailyWeather(res.data.daily);
+              });
+          }
+          else {
+            alert(`${curCity} invalid!`)
+          }
         });
     };
     fetchWeatherData();
   }, [curCity]);
   return (
     <div className='weather_screen'>
-      <SideBar curCity={curCity} handleCityChange={city=>setCurCity(city)} />
+      <SideBar
+        curCity={curCity}
+        handleCityChange={(city) => setCurCity(city)}
+      />
       <div className='weather_screen_container'>
         <WeatherCard curWeather={curWeather} curCity={curCity} />
         {[1, 2, 3, 4].map((i) => {
